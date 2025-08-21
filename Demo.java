dabde15db6fc8c6880ffbaf7b13170d9c57167e8
Dharanikama <dharanikama10@gmail.com>
2025-08-21 16:01:16 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo{
+	public static void main(String args[]){
+	int n=5;
+	for(int i=2;i<n;i++){
+		if(n%i==0){
+		System.out.println("not prime");
+		}
+		else{
+		System.out.println(" prime");
+		}
+	  }
+	}
+}
